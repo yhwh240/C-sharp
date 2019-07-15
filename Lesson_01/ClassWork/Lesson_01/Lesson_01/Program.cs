dc5188c88310ae30686e61e1ffedb231b6f4ef68
basalduk <47829188+basalduk@users.noreply.github.com>
2019-07-15 21:59:56 +0300

    Initial commit.

---
+++ Lesson_01/ClassWork/Lesson_01/Lesson_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;//Кодировка
+using System.Threading;//работа с потоками
+
+
+namespace Lesson_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Кодировка
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            Console.WriteLine("Enter your name: ");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Thread.Sleep(5000);// остановка потока на 5 сек
+            Console.WriteLine($"Hello, {name} !");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
