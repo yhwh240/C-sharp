c82caa53600771b38b210e7bb0fcdbb1ed1d0e57
yhwh240 <powerwhite102@gmail.com>
2019-07-16 23:10:33 +0300

    Homework of the 1st lesson added

---
+++ Lesson_01/HomeWork/Lesson_01_Home/Lesson_01_Home/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;//Кодировка
+using System.Threading;//работа с потоками
+
+
+namespace Lesson_01_Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Кодировка
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            Console.WriteLine("Enter your name: ");
+            Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Thread.Sleep(5000);// остановка потока на 5 сек
+            Console.WriteLine($"Hello, {name} !");
+            Console.WriteLine();
+            Thread.Sleep(5000);// остановка потока на 5 сек
+            Console.WriteLine($"Bye, {name} !");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
