6c2446b16e2a0f6d395bd38b792014c1f553e3c5
yhwh240 <powerwhite102@gmail.com>
2019-08-16 21:01:00 +0300

    HomeWork for lesson 4 and 5 added

---
+++ Lesson04/HomeWork/Lesson04HomeWorkColors/Lesson04HomeWorkColors/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Lesson04HomeWorkColors
+{
+    class Program
+    {
+        //Объявляем цвета
+        [Flags]
+        enum Colors
+        {
+            //None = 0x0,
+            Black = 0x1,
+            Blue = 0x1 << 1,
+            Cyan = 0x1 << 2,
+            Grey = 0x1 << 3,
+            Green = 0x1 << 4,
+            Magenta = 0x1 << 5,
+            Red = 0x1 << 6,
+            White = 0x1 << 7,
+            Yellow = 0x1 << 8
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //Объявляем переменные цвета и соотвествующего ему порядкового номера
+            Colors colorBlack = Colors.Black;
+            int numberBlack = (int)colorBlack;
+
+            Colors colorBlue = Colors.Blue;
+            int numberBlue = (int)colorBlue;
+
+            Colors colorCyan = Colors.Cyan;
+            int numberCyan = (int)colorCyan;
+
+            Colors colorGrey = Colors.Grey;
+            int numberGrey = (int)colorGrey;
+
+            Colors colorGreen = Colors.Green;
+            int numberGreen = (int)colorGreen;
+
+            Colors colorMagenta = Colors.Magenta;
+            int numberMagenta = (int)colorMagenta;
+
+            Colors colorRed = Colors.Red;
+            int numberRed = (int)colorRed;
+
+            Colors colorWhite = Colors.White;
+            int numberWhite = (int)colorWhite;
+
+            Colors colorYellow = Colors.Yellow;
+            int numberYellow = (int)colorYellow;
+
+            //Выводим список палитры цветов
+            Console.WriteLine("Please, select 4 colors, that you like, from the list of proposed: ");
+            Console.WriteLine("{0}.{1}", numberBlack, colorBlack);
+            Console.WriteLine("{0}.{1}", numberBlue, colorBlue);
+            Console.WriteLine("{0}.{1}", numberCyan, colorCyan);
+            Console.WriteLine("{0}.{1}", numberGrey, colorGrey);
+            Console.WriteLine("{0}.{1}", numberGreen, colorGreen);
+            Console.WriteLine("{0}.{1}", numberMagenta, colorMagenta);
+            Console.WriteLine("{0}.{1}", numberRed, colorRed);
+            Console.WriteLine("{0}.{1}", numberWhite, colorWhite);
+            Console.WriteLine("{0}.{1}", numberYellow, colorYellow);
+
+            //Помещаем все элементы в одну переменную
+            Colors allColors = 0;
+            allColors = Colors.Black | Colors.Blue | Colors.Cyan | Colors.Green | Colors.Grey | Colors.Magenta | Colors.Red | Colors.Yellow;
+
+            //Парсим ввод порядковых номеров цветов
+            Console.WriteLine();
+            Console.WriteLine("Select number of color:");
+            string inputColor = Console.ReadLine();
+            string inputColor2 = Console.ReadLine();
+            string inputColor3 = Console.ReadLine();
+            string inputColor4 = Console.ReadLine();
+            Colors inp1 = (Colors)Enum.Parse(typeof(Colors), inputColor);
+            Colors inp2 = (Colors)Enum.Parse(typeof(Colors), inputColor2);
+            Colors inp3 = (Colors)Enum.Parse(typeof(Colors), inputColor3);
+            Colors inp4 = (Colors)Enum.Parse(typeof(Colors), inputColor4);
+
+            //Выводим список понравившихся цветов
+            Console.WriteLine();
+            Console.WriteLine("List of favorite colors:");
+
+            //Создаём переменную "Избранное"
+            Colors favoriteColors = 0;
+            favoriteColors = inp1 | inp2 | inp3 | inp4;
+            //Console.WriteLine("{0}.{1}", inputColor, inp1);
+            //Console.WriteLine("{0}.{1}", inputColor2, inp2);
+            //Console.WriteLine("{0}.{1}", inputColor3, inp3);
+            //Console.WriteLine("{0}.{1}", inputColor4, inp4);
+
+            Console.WriteLine(string.Join("| ", new[] { favoriteColors }));
+
+            //Вычитаем понравившиеся цвета из переменной allColors и выводим оставшееся содержимое
+            Colors notFavoriteColors = 0;
+            notFavoriteColors = allColors ^ favoriteColors;
+            Console.WriteLine("List of color, that you not like: ");
+            Console.WriteLine(string.Join("| ", new[] { notFavoriteColors }));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson05/HomeWork/Lesson05HomeWork/Lesson05HomeWork/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Lesson05HomeWork
+{
+    class Program
+    {
+        //Объявляем список фигур
+        [Flags]
+        enum Figures {
+            Circle = 0x1,
+            Triangle = 0x1 <<1,
+            Rectangle = 0x1 <<2 }
+        static void Main(string[] args)
+        {
+            //Выводим список фигур и парсим выбор
+            Figures allFigures = Figures.Circle | Figures.Rectangle | Figures.Triangle;
+            Console.WriteLine("Please, choose the figures from list of proposed:");
+            Console.WriteLine(allFigures);
+            Figures inpFigures = Figures.Rectangle; //По умолчанию фигурой является прямоугольник
+
+            //Обрабатываем ошибку ввода фигуры
+            try
+            {
+                inpFigures = (Figures)Enum.Parse(typeof(Figures), Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("\n" + "This figures is not in the list");
+            }
+
+            Console.WriteLine("\n" + $"Your figures - {inpFigures}");
+
+            switch (inpFigures)
+            {
+                case Figures.Rectangle:
+                    double a = 1;
+                    double b = 1;
+                    Console.WriteLine("\n" + "Please, enter the length of the rectangle:");
+                    //Обрабатываем ошибку ввода данных
+                    try
+                    {
+                        a = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("\n" + "Error");
+                    }
+                    finally
+                    {
+                        a++;
+                    }
+ 
+                    Console.WriteLine("\n" + "Please, enter the height of the rectangle:");
+                    try
+                    {
+                        b = double.Parse(Console.ReadLine());
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("\n" + "Error");
+                    }
+                    finally
+                    {
+                        b++;
+                    }
+
+                    //Выводим данные и результаты вычислений
+                    Console.WriteLine("\n" + $"Length = {a}");
+                    Console.WriteLine("\n" + $"Height = {b}");
+                    Console.WriteLine("\n" + $"Square = {a * b}");
+                    Console.WriteLine("\n" + $"Perimeter = {2*(a + b)}");
+
+                    break;
+
+                case Figures.Circle:
+                    double r = 1;
+                    Console.WriteLine("\n" + "Please, enter the radius of circle:");
+                    //Обрабатываем ошибку ввода данных
+                    try
+                    {
+                        r = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("\n" + "Error");
+                    }
+                    finally
+                    {
+                        r++;
+                    }
+
+                    //Выводим данные и результаты вычислений
+                    Console.WriteLine("\n" + $"Radius = {r}");
+                    Console.WriteLine("\n" + $"Square = {Math.PI * Math.Pow(r, 2)}");
+                    Console.WriteLine("\n" + $"Perimetr = {2 * Math.PI * r}");
+
+                    break;
+
+                case Figures.Triangle:
+                    double c = 1;
+                    Console.WriteLine("\n" + "Please, enter the side of triangle:");
+                    //Обрабатываем ошибку ввода данных
+                    try
+                    {
+                        c = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("\n" + "Error");
+                    }
+                    finally
+                    {
+                        c++;
+                    }
+
+                    //Выводим данные и результаты вычислений
+                    Console.WriteLine("\n" + $"Side = {c}");
+                    Console.WriteLine("\n" + $"Square = {(Math.Pow(c, 2) * Math.Sqrt(3)) / 2}");
+                    Console.WriteLine("\n" + $"Perimetr = {3 * c}");
+
+                    break;
+                    
+
+            }
+
+            Console.WriteLine("\n" + "Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
