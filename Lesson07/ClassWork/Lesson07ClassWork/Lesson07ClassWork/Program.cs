e2cecf0515eb4286c935ef7366c0acdbe59d02de
Vladimir Kleymenov <undewrwatertales1976@gmail.com>
2019-08-19 22:18:02 +0300

    ClassWork of lesson 7 added

---
+++ Lesson07/ClassWork/Lesson07ClassWork/Lesson07ClassWork/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Lesson07ClassWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//Console.WriteLine(@"\tb\nc");
+
+			//DateTime now = DateTime.Now;
+			//DateTimeOffset nowOffset = DateTimeOffset.Now;
+
+			//Console.WriteLine(now + "\n" + nowOffset);
+			//string result = string.Format("Now is {0:dd MMMM yyyy HH:mm:ss}", now);
+			//Console.WriteLine(result);
+			//string result2 = string.Format("Now is {0}", now.ToString("dd MMMM yyyy HH:mm:ss"));
+			//Console.WriteLine(result2);
+
+
+			//TimeSpan magicTime = DateTime.Now - DateTime.Parse("05.07.1999");
+			//Console.WriteLine(magicTime.TotalDays);
+
+			//string result4 = string.Format($"Сейчас {now:dd MMMM yyyy HH:mm:ss ddd}");
+			//Console.WriteLine("\n"+result4);
+
+
+			//Самостоятельная
+			//Console.WriteLine("Please, enter two num:");
+			//double a = double.Parse(Console.ReadLine());
+			//double b = double.Parse(Console.ReadLine());
+
+			//Console.WriteLine("Your first num is {0}", a);
+			//Console.WriteLine("Your second num is {0}", b);
+
+			//Console.WriteLine(a + " * " + b + " = " + (a * b).ToString("0.0"));
+
+			//string result2 = string.Format("{0} + {1} = {2:0.0}", a, b, a + b);
+			//Console.WriteLine(result2);
+
+			//Console.WriteLine($"{a} - {b} = {a - b:0.0}");
+
+			//самостоятельная 2
+			//Console.WriteLine("Please, enter your word:");
+			//string word = Console.ReadLine();
+			//Console.WriteLine("\nPlease, enter letter for search:");
+			//string letter = Console.ReadLine();
+			//int i = 0;
+			//int j = -1;
+
+
+
+			//do
+			//{
+			//	i = word.IndexOf(letter, j+1);
+			//	Console.WriteLine("\n" + i);
+			//	j = i;
+			//} while (i >= 0);
+
+
+
+
+
+			Console.ReadKey();
+		}
+	}
+}
