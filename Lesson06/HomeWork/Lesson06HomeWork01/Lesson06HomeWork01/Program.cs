9bf701dfcb716d33e2c5330e867df542b930ee02
yhwh240 <powerwhite102@gmail.com>
2019-09-03 18:05:47 +0300

    Homework of the lesson06 added

---
+++ Lesson06/HomeWork/Lesson06HomeWork01/Lesson06HomeWork01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lesson06HomeWork01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            do
+            {
+                Console.WriteLine("\n Please, enter natural number, no more than 2 billion: ");
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Error!" +"\n" + e);
+                    continue;
+                }
+
+            } while (number == 0);
+
+            Console.WriteLine($"Your number is '{number}'");
+
+            string numbers = Convert.ToString(number);
+            int i = 0;
+
+            foreach(char num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Your number '{number}' contains {i} even numerals");
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson06/HomeWork/Lesson06HomeWork02/Lesson06HomeWork02/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Lesson06HomeWork02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double downPaymentAmount = 100;
+            double dailyIncomePercent = 0.0003;
+            double desiredAccumulationAmount = 200;
+
+            Console.WriteLine("Please, enter the amount of the down payment in rubles:");
+            try
+            {
+                downPaymentAmount = Double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error!" + "\n" + e);
+            }
+            finally
+            {
+                Console.WriteLine("\n" + $"Amount of the down payment is {downPaymentAmount} rub");
+            }
+
+
+            Console.WriteLine("\n" + "Please, enter the daily income percent, as a decimal fraction (1% = 0.01):");
+            try
+            {
+                dailyIncomePercent = Double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error!" + "\n" + e);
+            }
+            finally
+            {
+                Console.WriteLine("\n" + $"Daily income is {dailyIncomePercent}%");
+            }
+
+
+            Console.WriteLine("\n" + "Please enter the desired amount of savings in rubles:");
+            try
+            {
+                desiredAccumulationAmount = Double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error!" + "\n" + e);
+            }
+            finally
+            {
+                Console.WriteLine("\n" + $"Your desired amount of savings is {desiredAccumulationAmount} rub");
+            }
+
+            int days = 0;
+            double count = downPaymentAmount;
+
+            do
+            {
+                count  += (count * dailyIncomePercent);
+                days++;
+
+            } while (count <= desiredAccumulationAmount);
+
+            Console.WriteLine("\n" + $"The required number of days to accumulate the desired amount: {days}");
+            Console.ReadKey();
+        }
+    }
+}
