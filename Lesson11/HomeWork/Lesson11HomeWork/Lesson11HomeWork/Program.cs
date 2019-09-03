0b0338784a97bffaf2e0c855cd022c57e8afabbc
yhwh240 <powerwhite102@gmail.com>
2019-09-03 21:25:35 +0300

    Homework of the lesson11 added

---
+++ Lesson11/HomeWork/Lesson11HomeWork/Lesson11HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson11HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day1 = "2012-03-01 00:00:00";
+            var day2 = "2019-10-18 15:20:00";
+            ReminderItem item1 = new ReminderItem(DateTimeOffset.Parse(day1), "AlarmClock 1");
+            Console.WriteLine(item1.WriteProperties());
+
+            ReminderItem item2 = new ReminderItem(DateTimeOffset.Parse(day2), "AlarmClock 2");
+            Console.WriteLine("\n" + item2.WriteProperties());
+
+
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson11/HomeWork/Lesson11HomeWork/Lesson11HomeWork/ReminderItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson11HomeWork
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return DateTime.Now - AlarmDate;
+            }
+        }
+        public bool IsOutdated
+        {
+            get
+            {
+                if (DateTime.Now >= AlarmDate)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public ReminderItem(DateTimeOffset date, string message)
+        {
+            AlarmDate = date;
+            AlarmMessage = message;
+        }
+
+        public string WriteProperties()
+        {
+            return $"AlarmDate: {AlarmDate}" + "\n" +
+                $"AlarmMessage: {AlarmMessage}" + "\n" +
+                $"TimeToAlarm: {TimeToAlarm}" + "\n" +
+                $"IsOutDate: {IsOutdated} ";
+        }
+    }
+}
