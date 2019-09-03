7b4c29a2c90ff9f09c3b777f4d3a513bea4ea14e
yhwh240 <powerwhite102@gmail.com>
2019-09-03 18:54:05 +0300

    HomeWork of the lesson10 added

---
+++ Lesson10/HomeWork/Lesson10HomeWork/Lesson10HomeWork/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson10HomeWork
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int AgeAfter4Years
+        {
+            get
+            {
+                return Age += 4;
+            }
+        }
+
+        public string Descriprion
+        {
+            get
+            {
+                return $"Name: {Name}, Age after 4 years : {AgeAfter4Years}";
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Lesson10/HomeWork/Lesson10HomeWork/Lesson10HomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson10HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Person[] people = new Person[3];
+            for (int i = 0; i < 3; i++)
+            {
+                people[i] = new Person();
+                Console.WriteLine($"Enter name {i}: ");
+                people[i].Name = Console.ReadLine();
+                Console.WriteLine($"Enter age {i}: ");
+                try
+                {
+                    people[i].Age = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Error!" + "\n" + e);
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(people[i].Descriprion);
+            }
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
